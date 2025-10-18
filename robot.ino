75b1aadcf29a2cb01e28266d13cf10ac4bbe0f6d
DanteEspino4D <dantealerojas45@gmail.com>
2025-10-17 19:07:12 -0600

    tarea de los motores creada

--- robot.ino
+++ robot.ino
@@ -7,10 +7,22 @@
 // direccion del multiplexor
 #define TCAADDR 0x70
 
+// variables de la configuracion de los pines pwm
+int freq = 5000;
+int solut = 8;
+int pwmC_1 = 0;
+int pwmC_2 = 1;
+
 // variables que establecen el tiemṕo
 int tiempo1 = 2000; // tiempo que sigue avanzando despues de dejar de detectar
                     // al rival
 int tiempo2 = 2000; // tiempo que retrocede al detectar el borde
+
+// variables que cuentan el tiempo
+unsigned long temp1 = 0;
+unsigned long temp2 = 0;
+unsigned long temp3 = 0;
+unsigned long temp4 = 0;
 
 // variables que definen limites
 int maxd = 40;    // limite de los sensores ultrasonicos
@@ -23,6 +35,9 @@ SemaphoreHandle_t alerta2;
 // alerta de deteccion del rival
 SemaphoreHandle_t enemigo;
 SemaphoreHandle_t enemigo2;
+
+// orden de una accion
+QueueHandle_t orden;
 
 // variables de control
 bool estado = false;
@@ -42,8 +57,8 @@ int trig_2 = 12;
 int echo_2 = 35;
 int led_1 = 19;
 int led_2 = 5;
-int pwm_A1 = 4;
-int pwm_A2 = 18;
+int pwm_1 = 4;
+int pwm_2 = 18;
 int cal = 15;
 int ledp_1 = 16;
 int ledp_2 = 17;
@@ -69,6 +84,20 @@ NewPing ojos_2(trig_2, echo_2, maxd);
 // valores establecidos del limite(pasar mas tarde a variables)
 uint16_t lcr = redC, lcg = green, lcb = blue;
 
+// struct con los valores de cada movimiento de los motores
+struct MotorAccion {
+  int vel_1;
+  int vel_2;
+  int e[2][2];
+};
+
+// se crea la variable struct de la direccion a, b y giro
+MotorAccion dir_a = {255, 255, {{1, 0}, {1, 0}}};
+
+MotorAccion dir_b = {255, 255, {{0, 1}, {0, 1}}};
+
+MotorAccion giro = {255, 255, {{0, 1}, {1, 0}}};
+
 // funcion para probar el funcionamiento de cosas
 void prueba(int a) {
   if (a == 0) {
@@ -79,24 +108,7 @@ void prueba(int a) {
     delay(1000);
   }
 }
-// funcion que avanza en la direccion a(por definir en el robot fisico)
-void dir_a() {
-  alto();
-  digitalWrite(mot[0][0], HIGH);
-  digitalWrite(mot[1][0], HIGH);
-}
-// funcion que avanza en la direccion b(por definir en el robot fisico)
-void dir_b() {
-  alto();
-  digitalWrite(mot[1][1], HIGH);
-  digitalWrite(mot[0][1], HIGH);
-}
-// gira al robot
-void giro() {
-  alto();
-  digitalWrite(mot[1][0], HIGH);
-  digitalWrite(mot[0][1], HIGH);
-}
+
 // para el robot
 void alto() {
   digitalWrite(mot[0][0], LOW);
@@ -141,118 +153,133 @@ void calCol() {
 // TAREA DE LA LOGICA DEL ROBOT
 
 void robot(void *pvParameters) {
+  // prende al precionar el boton
+  while (digitalRead(ini) == LOW) {
+    start = true;
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+  }
   while (1) {
-    unsigned long temp1 = 0;
-    unsigned long temp2 = 0;
-    unsigned long temp3 = 0;
-    unsigned long temp4 = 0;
-
-    // prende al precionar el boton
-    if (digitalRead(ini) == HIGH) {
-      start = true;
-      vTaskDelay(5000 / portTICK_PERIOD_MS);
-    }
     // inicia
-    while (start) {
 
-      // condiciones que evaluan si ya pasaron los tiempos
-      if (millis() - temp1 >= tiempo1) {
-        memo1 = false;
-      }
-      if (millis() - temp2 >= tiempo1) {
-        memo2 = false;
-      }
-      if (millis() - temp3 >= tiempo2) {
-        memo3 = false;
-      }
-      if (millis() - temp4 >= tiempo2) {
-        memo4 = false;
-      }
-
-      // MAQUINA DE ESTADOS
-
-      // selecciona el estado
-
-      // si detecta el limite por sc_1
-      if (xSemaphoreTake(alerta, 0) == pdTRUE || memo3) {
-        modo = 0;
-      }
-      // si detecta el limite por sc_2
-      else if (xSemaphoreTake(alerta2, 0) == pdTRUE || memo4) {
-        modo = 1;
-      }
-      // si deja de detectar al robot por ojos 1
-      else if (xSemaphoreTake(enemigo, 0)) {
-        modo = 2;
-      }
-      // si deja de detectar al robot por ojos 2
-      else if (xSemaphoreTake(enemigo2, 0)) {
-        modo = 3;
-      }
-      // si detecta el robot por ojos 1
-      else if (memo1) {
-        modo = 4;
-      }
-      // si detecta el robot por ojos 2
-      else if (memo2) {
-        modo = 5;
-      }
-      // si no detecta nada
-      else {
-        modo = 6;
-      }
-
-      // ejecuta el estado
-      switch (modo) {
-      // detiene el movimiento y retrocede en direccion b
-      case 0:
-        dir_b();
-        memo3 = true;
-        temp4 = millis();
-        break;
-      // detiene el movimiento y retrocede en direccion a
-      case 1:
-        dir_a();
-        memo4 = true;
-        temp3 = millis();
-        break;
-      // avanza por un tiempo definido de 4 segundo en direccion a
-      case 2:
-        dir_a();
-        memo1 = true;
-        temp1 = millis();
-        temp3 = millis();
-        temp4 = millis();
-        break;
-      // avanza por un tiempo definido de 4 segundos en direccion b
-      case 3:
-        dir_b();
-        memo2 = true;
-        temp2 = millis();
-        temp3 = millis();
-        temp4 = millis();
-        break;
-      // avanza en direccion a
-      case 4:
-        dir_a();
-        temp3 = millis();
-        temp4 = millis();
-        break;
-      // avanza en direccion b
-      case 5:
-        dir_b();
-        temp3 = millis();
-        temp4 = millis();
-        break;
-      // da vueltas hasta encontrar el robot
-      case 6:
-        giro();
-        temp3 = millis();
-        temp4 = millis();
-        break;
-      }
-      vTaskDelay(10 / portTICK_PERIOD_MS);
+    // condiciones que evaluan si ya pasaron los tiempos
+    if (millis() - temp1 >= tiempo1) {
+      memo1 = false;
     }
+    if (millis() - temp2 >= tiempo1) {
+      memo2 = false;
+    }
+    if (millis() - temp3 >= tiempo2) {
+      memo3 = false;
+    }
+    if (millis() - temp4 >= tiempo2) {
+      memo4 = false;
+    }
+
+    // MAQUINA DE ESTADOS
+
+    // selecciona el estado
+
+    // si detecta el limite por sc_1
+    if (xSemaphoreTake(alerta, 0) == pdTRUE || memo3) {
+      modo = 0;
+    }
+    // si detecta el limite por sc_2
+    else if (xSemaphoreTake(alerta2, 0) == pdTRUE || memo4) {
+      modo = 1;
+    }
+    // si deja de detectar al robot por ojos 1
+    else if (xSemaphoreTake(enemigo, 0)) {
+      modo = 2;
+    }
+    // si deja de detectar al robot por ojos 2
+    else if (xSemaphoreTake(enemigo2, 0)) {
+      modo = 3;
+    }
+    // si detecta el robot por ojos 1
+    else if (memo1) {
+      modo = 4;
+    }
+    // si detecta el robot por ojos 2
+    else if (memo2) {
+      modo = 5;
+    }
+    // si no detecta nada
+    else {
+      modo = 6;
+    }
+
+    // ejecuta el estado
+    switch (modo) {
+    // detiene el movimiento y retrocede en direccion b
+    case 0:
+      xQueueSend(orden, &dir_b, 0);
+      memo3 = true;
+      temp4 = millis();
+      break;
+      // detiene el movimiento y retrocede en direccion a
+    case 1:
+      xQueueSend(orden, &dir_a, 0);
+      memo4 = true;
+      temp3 = millis();
+      break;
+    // avanza por un tiempo definido de 4 segundo en direccion a
+    case 2:
+      xQueueSend(orden, &dir_a, 0);
+      memo1 = true;
+      temp1 = millis();
+      temp3 = millis();
+      temp4 = millis();
+      break;
+    // avanza por un tiempo definido de 4 segundos en direccion b
+    case 3:
+      xQueueSend(orden, &dir_b, 0);
+      memo2 = true;
+      temp2 = millis();
+      temp3 = millis();
+      temp4 = millis();
+      break;
+    // avanza en direccion a
+    case 4:
+      xQueueSend(orden, &dir_a, 0);
+      temp3 = millis();
+      temp4 = millis();
+      break;
+    // avanza en direccion b
+    case 5:
+      xQueueSend(orden, &dir_b, 0);
+      temp3 = millis();
+      temp4 = millis();
+      break;
+      // da vueltas hasta encontrar el robot
+    case 6:
+      xQueueSend(orden, &giro, 0);
+      temp3 = millis();
+      temp4 = millis();
+      break;
+    }
+    vTaskDelay(10 / portTICK_PERIOD_MS);
+  }
+}
+
+// TAREA DE LOS MOTORES
+
+void motores(void *pvPrarmeters) {
+  for (;;) {
+    // se crea la variable donde se guardara la acion;
+    MotorAccion accion;
+
+    // se recive la accion
+    xQueueReceive(orden, &accion, portMAX_DELAY);
+
+    // se para todo, aplica velocidades y se encienden los que motores que se
+    // necesitan
+    alto();
+    ledcWrite(pwmC_1, accion.vel_1);
+    ledcWrite(pwmC_2, accion.vel_2);
+    digitalWrite(mot[0][0], accion.e[0][0]);
+    digitalWrite(mot[0][1], accion.e[0][1]);
+    digitalWrite(mot[1][0], accion.e[1][0]);
+    digitalWrite(mot[1][1], accion.e[1][1]);
   }
 }
 
@@ -329,6 +356,10 @@ void setup() {
   // se crean las alertas de deteccion de enemigos
   enemigo = xSemaphoreCreateBinary();
   enemigo2 = xSemaphoreCreateBinary();
+
+  // se crea la la orden con la que se estara comunicando con los motores
+  orden = xQueueCreate(5, sizeof(MotorAccion));
+
   // configuaracion de pines de los sensores de color
   Wire.begin();
 
@@ -344,11 +375,19 @@ void setup() {
   pinMode(23, OUTPUT);
   pinMode(ledp_1, OUTPUT);
   pinMode(ledp_2, OUTPUT);
+
   //  se inicializan los pines de los motores(puentes h)
   pinMode(mot[0][0], OUTPUT);
   pinMode(mot[0][1], OUTPUT);
   pinMode(mot[1][0], OUTPUT);
   pinMode(mot[1][1], OUTPUT);
+
+  // configuracion y asignacion de los pines pwm
+  ledcSetup(pwmC_1, freq, solut);
+  ledcSetup(pwmC_2, freq, solut);
+  ledcAttachPin(pwm_1, pwmC_1);
+  ledcAttachPin(pwm_2, pwmC_2);
+
   // selecciona sc_1
   scSel(0);
   // verifica el funcionamiento de sc_1
@@ -374,18 +413,11 @@ void setup() {
   }
   calCol();
 
-  // aun no tiene propocito(determinara la calibracion)
-  if (digitalRead(cal) == LOW) {
-    calCol();
-  } else {
-    // valores predeterminados del sensor de color
-    lcr = redC;
-    lcg = green;
-    lcb = blue;
-  }
   // se crean las tareas
-  xTaskCreatePinnedToCore(robot, "robot", 1024, NULL, 1, NULL, 1);
-  xTaskCreatePinnedToCore(senColor, "sensorColor", 2048, NULL, 1, NULL, 0);
+  xTaskCreatePinnedToCore(robot, "robot", 1024, NULL, 2, NULL, 1);
+  xTaskCreatePinnedToCore(motores, "motores", 1024, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(senColor, "sensorColor", 2048, NULL, 3, NULL, 0);
+  xTaskCreatePinnedToCore(senUltra, "SensorUltra", 2048, NULL, 3, NULL, 0);
   xTaskCreatePinnedToCore(musica, "musica", 1024, NULL, 1, NULL, 0);
 }
 void loop() {
