4726a5d4cfe101cd4e51ea89bdb5715fd1e7e7d2
DanteEspino4D <dantealerojas45@gmail.com>
2025-10-29 19:56:46 -0600

    clase SensorRival en espidf

--- src/SensorRival.cpp
+++ src/SensorRival.cpp
@@ -1,11 +1,10 @@
-#include <Arduino.h>
-#include <NewPing.h>
+#include "esp_timer.h"
+#include "freertos/task.h"
 #include "SensorRival.h"
+#include "driver/gpio.h"
 
-SensorRival::SensorRival(int _maxd, int _trig_1, int _echo_1, int _trig_2, int _echo_2):
+SensorRival::SensorRival(int _maxd, gpio_num_t _trig_1, gpio_num_t _echo_1, gpio_num_t _trig_2, gpio_num_t _echo_2):
 	maxd(_maxd),
-	ojos_1(_trig_1, _echo_1, _maxd),
-	ojos_2(_trig_2, _echo_2, _maxd),
 	trig_1(_trig_1),
 	trig_2(_trig_2),
 	echo_1(_echo_1),
@@ -13,12 +12,45 @@ SensorRival::SensorRival(int _maxd, int _trig_1, int _echo_1, int _trig_2, int _
 
 {}
 
+uint32_t SensorRival::dist_cm(gpio_num_t trig, gpio_num_t echo){
+	//configuracion inicial
+	gpio_set_level(trig, 0);
+	ets_delay_us(2);
+
+	//pulso trigger
+	gpio_set_level(trig, 1);
+	ets_delay_us(10);
+	gpio_set_level(trig, 0);
+
+	//detectar por echo
+	uint32_t timeout_us = 50000;
+	int64_t startT = esp_timer_get_time();
+	while(gpio_get_level(echo) == 0){
+		if((esp_timer_get_time() - startT) > timeout_us){
+			return 0;
+		}
+	}
+
+	//mide el tiempo en el que se detecta por echo
+	startT = esp_timer_get_time();
+	while(gpio_get_level(echo) == 1){
+		if((esp_timer_get_time() - startT) > timeout_us){
+			return 0;
+		}
+	}
+	//calcula la distacia
+	uint64_t dur = esp_timer_get_time() - startT;
+	uint32_t dis = dur / 58;
+	//retorna la distancia
+	return dis;
+}
+
 bool SensorRival::ojos_1Verify(){
-	unsigned int dis = ojos_1.ping_cm();
+	unsigned int dis = dist_cm(trig_1, echo_1);
 	return (dis > 0 && dis <= maxd);
 }
 
 bool SensorRival::ojos_2Verify(){
-	unsigned int dis = ojos_2.ping_cm();
+	unsigned int dis = dist_cm(trig_2, echo_2);
 	return (dis > 0 && dis <= maxd);
 }
--- src/SensorRival.h
+++ src/SensorRival.h
@@ -1,25 +1,24 @@
 #ifndef SENSORRIVAL_H
 #define SENSORRIVAL_H
-#include <Arduino.h>
-#include <NewPing.h>
 
+#include "driver/gpio.h"
 class SensorRival{
 	private:
-		// se crean objetos ojos_1 y ojos_2
-		NewPing ojos_1;
-		NewPing ojos_2;
-		//variables de pines
-		int trig_1;
-		int trig_2;
-		int echo_1;
-		int echo_2;
+		//pines de los sensores
+		gpio_num_t trig_1;
+		gpio_num_t trig_2;
+		gpio_num_t echo_1;
+		gpio_num_t echo_2;
+
+		//metodo para medir distancia
+		uint32_t dist_cm(gpio_num_t trig_pin, gpio_num_t echo_pin);
 
 		//limite de distancia
 		int maxd;
 
 	public:
 		//costructor
-		SensorRival(int _maxd, int _trig_1, int _echo_1, int _trig_2, int _echo_2);
+		SensorRival(int _maxd, gpio_num_t _trig_1, gpio_num_t _echo_1, gpio_num_t _trig_2, gpio_num_t _echo_2);
 
 		//metodos de verificacion
 		bool ojos_1Verify();
