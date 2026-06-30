59deb4c9420db8a4b5792f79d69c97a857f171f5
DanteEspino4D <dantealerojas45@gmail.com>
2026-06-30 15:04:54 -0600

    funcion que establece la velocidad
    se creo la funcion que establece la velocidad en la clase de los motores
    del STM32G4RET6

--- stm32g4/Core/Src/Motores.cpp
+++ stm32g4/Core/Src/Motores.cpp
@@ -18,4 +18,50 @@ Motores::Motores(TIM_HandleTypeDef *htim):
 	vel_hD(950)
 {}
 
+//se inicializa todo
+void Motores::begin(){
+	//inicia el timer para generar la señal PWM
+	HAL_TIM_PWM_Start(htim1, TIM_CHANNEL_1);
+	HAL_TIM_PWM_Start(htim1, TIM_CHANNEL_2);
+	HAL_TIM_PWM_Start(htim1, TIM_CHANNEL_3);
+	HAL_TIM_PWM_Start(htim1, TIM_CHANNEL_4);
+}
 
+//estavlece las velocidades
+void Motores::velocidad(int16_t vel_1, int16_t vel_2, bool ram){
+	//detecta si se usara rampa
+	if(ram){
+		//futura rampa de aceleracion con CORDIC
+	}
+
+	//se establecen las velocidades de los motores de la izquierda
+	if(vel_1 < 0){
+		//si es negativa
+		vel_1 = -vel_1;
+		__HAL_TIM_SET_COMPARE(htim1, TIM_CHANNEL_1, 0);
+		__HAL_TIM_SET_COMPARE(htim1, TIM_CHANNEL_2, vel_1);
+	}else if(vel_1 > 0){
+		//si es positiva
+		__HAL_TIM_SET_COMPARE(htim1, TIM_CHANNEL_1, vel_1);
+		__HAL_TIM_SET_COMPARE(htim1, TIM_CHANNEL_2, 0);
+	}else{
+		//freno
+		__HAL_TIM_SET_COMPARE(htim1, TIM_CHANNEL_1, 1023);
+		__HAL_TIM_SET_COMPARE(htim1, TIM_CHANNEL_2, 1023);
+	}
+
+	//se establecen las velocidades de los motores de la derecha
+	if(vel_2 < 0){
+		//si es negativa
+		vel_2 = -vel_2;
+		__HAL_TIM_SET_COMPARE(htim1, TIM_CHANNEL_3, 0);
+		__HAL_TIM_SET_COMPARE(htim1, TIM_CHANNEL_4, vel_2);
+	}else if(vel_2 > 0){
+		__HAL_TIM_SET_COMPARE(htim1, TIM_CHANNEL_3, vel_2);
+		__HAL_TIM_SET_COMPARE(htim1, TIM_CHANNEL_4, 0);
+	}else{
+		//freno
+		__HAL_TIM_SET_COMPARE(htim1, TIM_CHANNEL_3, 1023);
+		__HAL_TIM_SET_COMPARE(htim1, TIM_CHANNEL_4, 1023);
+	}
+}
