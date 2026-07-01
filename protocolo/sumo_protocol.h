666dd56f33a376195caaf27937e7909cc162928b
DanteEspino4D <dantealerojas45@gmail.com>
2026-07-01 15:13:09 -0600

    creacion de los structs de comunicacion
    se crearon los structs de comunicacion entre el STM32G474RET6 y el
    ESP32-S3 incluyendo el de configuracion

--- protocolo/sumo_protocol.h
+++ protocolo/sumo_protocol.h
@@ -6,20 +6,77 @@
 #pragma pack(push, 1)
 
 typedef struct {
-    uint8_t estado_maquina;
-    int16_t setpoint_izq;
-    int16_t setpoint_der;
-    uint8_t flag_linea;
-    uint8_t checksum;
-} MasterToSlave_Msg_t;
+	//verificador del inicio de la configuracion
+	uint8_t inicio[2];
+
+	//identificador del mensaje
+	uint8_t id;
+
+	//umbral de la deteccion del stall
+	uint16_t u_stall;
+
+	//tiempo de la rampa
+	uint8_t t_ram;
+
+	//velocidadees
+	int16_t normal_i;
+	int16_t normal_d;
+	int16_t ataque_i;
+	int16_t ataque_d;
+	int16_t pronunciado_i;
+	int16_t pronunciado_d;
+	int16_t maximo_i;
+	int16_t maximo_d;
+	int16_t giro_i;
+	int16_t giro_d;
+	int16_t evasion_i;
+	int16_t evasion_d;
+	int16_t huida_i;
+	int16_t huida_d;
+
+	//umbral de los TCRT
+	int16_t u_limite;
+
+	//verificador del final de la configuracion
+	uint8_t final;
+} Conf_t;
 
 typedef struct {
-    uint8_t estado_maquina;
-    int16_t setpoint_izq;
-    int16_t setpoint_der;
-    uint8_t flag_linea;
-    uint8_t checksum;
-} SlaveToMaster_Msg_t;
+	//verificador del inicio de la accion
+	uint8_t inicio[2];
+
+	//identificador del mensaje
+	uint8_t id;
+
+	//accion de los motores
+    uint8_t accion;
+
+	//contador
+	uint8_t cont;
+
+	//verificador del final de la accion
+	uint8_t final;
+} Esp_t;
+
+typedef struct {
+	//verificador del inicio de la transmision
+	uint8_t inicio[2];
+
+	//identificador del mensaje
+	uint8_t id;
+
+	//valores de los TCRT5000
+    uint8_t tcrt_1;
+	uint8_t tcrt_2;
+	uint8_t tcrt_3;
+	uint8_t tcrt_4;
+
+	//contador
+	uint8_t cont;
+
+	//verificador del final de la transmision
+	uint8_t final;
+} Stm_t;
 
 #pragma pack(pop)
 
--- stm32g4/Core/Src/app_main.cpp
+++ stm32g4/Core/Src/app_main.cpp
@@ -4,10 +4,13 @@
 
 void app_main(void)
 {
+	//se establece la accioninicial como en alto
 	uint8_t accion = 0;
+	//se crea el objetp de los motores
 	Motores mot(&htim1);
 	while (1)
 	{
+		//se manda la nueva accion
 		mot.controlador(accion);
 	}
 }
